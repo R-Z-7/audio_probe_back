6a872a91651fa71c084248a548d00c7697526d16
Ramezz <47709261+R-Z-7@users.noreply.github.com>
2024-05-13 00:02:28 +0100

    Update jwt.js

--- config/jwt.js
+++ config/jwt.js
@@ -1,4 +1,4 @@
-const secret = process.env.JWT_SECRET;
+const secret = dskfjgjkdsgfukjsdkjliauhi8yh3w8yrhubdsc78q264cgebewhcy7e6376aqdsifh67wi45wc6dszfisufuigadyfcyhgzsdbcbjhzxdcvjhgsduy6tgf6ugsehfcvdghczsdfygcfgjzsdhbcfhzsgxvgcvghszdc;
 const ttl = '144h';
 
 module.exports = {

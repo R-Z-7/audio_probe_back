9cfcf9c9016e8b92bdc4fcc597a5f45818c5405c
Ramezz <47709261+R-Z-7@users.noreply.github.com>
2024-05-14 23:25:07 +0100

    Update analysis.controller.js

--- routes/analysis/analysis.controller.js
+++ routes/analysis/analysis.controller.js
@@ -106,7 +106,7 @@ const deleteAnalysis = async (req, res) => {
 const getAudioAnalysis = async (req, res) => {
     const audioFilePath = path.join(__dirname,'audios');
     const praatFilePath = path.join(__dirname, 'praat-scripts');
-    const praatPath = 'praat '; 
+    const praatPath = `${praatFilePath+"/praat "}`; 
     try {
         console.log(path.join(audioFilePath, req.file.filename));
         const command = `${praatPath} ${praatFilePath+"/t10.praat"+" "+audioFilePath+"/"}`;

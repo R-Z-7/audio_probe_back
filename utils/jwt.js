91763b7aa383fdc7ee774c099d45ed3966db26de
Ramezz <47709261+R-Z-7@users.noreply.github.com>
2024-05-14 19:46:03 +0100

    Update jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtConfig = require('../config/jwt');
 
-exports.verifyToken = (token) => jwt.verify(token, jwtConfig.secret);
+exports.verifyToken = (token) => jwt.verify(token, "dskfjgjkdsgfukjsdkjliauhi8yh3w8yrhubdsc78q264cgebewhcy7e6376aqdsifh67wi45wc6dszfisufuigadyfcyhgzsdbcbjhzxdcvjhgsduy6tgf6ugsehfcvdghczsdfygcfgjzsdhbcfhzsgxvgcvghszdc");
 
-exports.createToken = (data) => jwt.sign(data, jwtConfig.secret, { expiresIn: jwtConfig.ttl });
+exports.createToken = (data) => jwt.sign(data, "dskfjgjkdsgfukjsdkjliauhi8yh3w8yrhubdsc78q264cgebewhcy7e6376aqdsifh67wi45wc6dszfisufuigadyfcyhgzsdbcbjhzxdcvjhgsduy6tgf6ugsehfcvdghczsdfygcfgjzsdhbcfhzsgxvgcvghszdc", { expiresIn: "144h" });
